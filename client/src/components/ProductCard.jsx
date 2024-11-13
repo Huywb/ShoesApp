65253106a932b2c8bef250a486e2805690902ee0
Huywb <pthhuy1306@gmail.com>
2024-11-13 17:00:06 +0700

    Products Page Update

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
+import logo from "../../src/assets/upload.svg";
+import featured from "../assets/featured.svg";
+import unfeatured from "../assets/unfeatured.svg";
+import delete1 from "../assets/delete1.svg";
+
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <div className="bg-[#1f2936] flex gap-2  p-4 px-8 text-lg">
+          <div className="flex-[3] flex gap-2 items-center">
+            <img src={logo} alt="" className="w-16"/>
+            <p>Test</p>
+          </div>
+          <span className="flex-[2] flex justify-center items-center">$123.00</span>
+          <span className="flex-[2] flex justify-center items-center">Test</span>
+          <span className="flex-[2] flex justify-center items-center">1234</span>
+          <span className="flex-[2] justify-center flex">
+            <img src={featured} alt="" className=" w-10 text-red-600 " />
+          </span>
+          <span className="flex-[2] justify-center flex">
+            <img src={delete1} alt=""  className=" w-10"/>
+          </span>
+        </div>
   )
 }
 
--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from "react";
+import ProductCard from "./ProductCard";
 
 const ProductList = () => {
   return (
-    <div className='w-screen flex justify-center'>
-      <div className='flex flex-col gap-2 w-[70%] bg-[#3a4152] p-4 px-8 '>
-        <div className='flex gap-2'>
-          <span className='flex-[3]'>PRODUCT</span>
-          <span className='flex-[2] text-center'>PRICE</span>
-          <span className='flex-[2] text-center'>CATEGORY</span>
-          <span className='flex-[2] text-center'>IN STOCK</span>
-          <span className='flex-[2] text-center'>FEATURED</span>
-          <span className='flex-[2] text-center'>ACTIONS</span>
+    <div className="w-screen flex justify-center ">
+      <div className="flex flex-col w-[70%] ">
+        <div className="flex gap-2 bg-[#3a4152] p-4 px-8 text-lg">
+          <span className="flex-[3]">PRODUCT</span>
+          <span className="flex-[2] text-center">PRICE</span>
+          <span className="flex-[2] text-center">CATEGORY</span>
+          <span className="flex-[2] text-center">IN STOCK</span>
+          <span className="flex-[2] text-center">FEATURED</span>
+          <span className="flex-[2] text-center">ACTIONS</span>
         </div>
+        <ProductCard></ProductCard>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
